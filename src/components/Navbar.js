17b2c0ca42cd9d9013f37a6cd1c41707fd67e787
sahilborse18 <sahilborse.909098@gmail.com>
2023-07-15 18:43:19 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,10 @@ export default function Navbar(change) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page"  href="/">Home</a>
+                  <a className="nav-link active" aria-current="page"  href="#">Home</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link"  href="/about">About</a>
+                  <a className="nav-link"  href="#">About</a>
                 </li>
               </ul>
               {/* <form className="d-flex" role="search">
